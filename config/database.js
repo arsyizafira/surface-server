7a384b2a3b52c7431b1c7a23e1eb9716e9cd5e82
brilian <briliantpmw@gmai.com>
2021-01-15 20:59:13 +0700

    [up] change databases

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://127.0.0.1:27017/sistem_pakar',
-  // database: 'mongodb+srv://brilian:123Pubz123@brilian.kyt5l.mongodb.net/sistem_pakar?authSource=admin&replicaSet=atlas-6i7mit-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
+  // database: 'mongodb://127.0.0.1:27017/sistem_pakar',
+  database: 'mongodb+srv://brilian:123Pubz123@brilian.kyt5l.mongodb.net/sistem_pakar?authSource=admin&replicaSet=atlas-6i7mit-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
 }
